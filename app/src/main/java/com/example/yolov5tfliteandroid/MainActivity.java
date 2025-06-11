4169ea2a5b15a58c38c0e48c701d164987e8fee2
JING <75834603+jiajinghong@users.noreply.github.com>
2025-06-11 22:47:54 +0800

    Initial commit - my custom YOLOv5 cat detector

---
+++ app/src/main/java/com/example/yolov5tfliteandroid/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.yolov5tfliteandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.camera.view.PreviewView;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.camera.lifecycle.ProcessCameraProvider;
+
+import com.example.yolov5tfliteandroid.analysis.FullImageAnalyse;
+import com.example.yolov5tfliteandroid.analysis.FullScreenAnalyse;
+import com.example.yolov5tfliteandroid.detector.Yolov5TFLiteDetector;
+import com.example.yolov5tfliteandroid.utils.CameraProcess;
+import com.google.common.util.concurrent.ListenableFuture;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean IS_FULL_SCREEN = false;
+
+    private PreviewView cameraPreviewMatch;
+    private PreviewView cameraPreviewWrap;
+    private ImageView boxLabelCanvas;
+    private Spinner modelSpinner;
+    private Switch immersive;
+    private TextView inferenceTimeTextView;
+    private TextView frameSizeTextView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private Yolov5TFLiteDetector yolov5TFLiteDetector;
+
+    private CameraProcess cameraProcess = new CameraProcess();
+
+    /**
+     * 获取屏幕旋转角度,0表示拍照出来的图片是横屏
+     *
+     */
+    protected int getScreenOrientation() {
+        switch (getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_270:
+                return 270;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_90:
+                return 90;
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * 加载模型
+     *
+     * @param modelName
+     */
+    private void initModel(String modelName) {
+        // 加载模型
+        try {
+            this.yolov5TFLiteDetector = new Yolov5TFLiteDetector();
+            this.yolov5TFLiteDetector.setModelFile(modelName);
+//            this.yolov5TFLiteDetector.addNNApiDelegate();
+            this.yolov5TFLiteDetector.addGPUDelegate();
+            this.yolov5TFLiteDetector.initialModel(this);
+            Log.i("model", "Success loading model" + this.yolov5TFLiteDetector.getModelFile());
+        } catch (Exception e) {
+            Log.e("image", "load model error: " + e.getMessage() + e.toString());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        // 全屏畫面
+        cameraPreviewMatch = findViewById(R.id.camera_preview_match);
+        cameraPreviewMatch.setScaleType(PreviewView.ScaleType.FILL_START);
+
+        // 全圖畫面
+        cameraPreviewWrap = findViewById(R.id.camera_preview_wrap);
+//        cameraPreviewWrap.setScaleType(PreviewView.ScaleType.FILL_START);
+
+        // box/label畫面
+        boxLabelCanvas = findViewById(R.id.box_label_canvas);
+
+        // 模型選擇按鈕
+        modelSpinner = findViewById(R.id.model);
+
+        // 沉浸體驗按鈕
+        immersive = findViewById(R.id.immersive);
+
+        // 即時顯示速度
+        inferenceTimeTextView = findViewById(R.id.inference_time);
+        frameSizeTextView = findViewById(R.id.frame_size);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        // 相機權限
+        if (!cameraProcess.allPermissionsGranted(this)) {
+            cameraProcess.requestPermissions(this);
+        }
+
+        // 獲取手機相機旋轉參數
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        Log.i("image", "rotation: " + rotation);
+
+        cameraProcess.showCameraSupportSize(MainActivity.this);
+
+        // 初始化加載模型，預設yolov5s優先
+        initModel("yolov5s");
+
+        // 监听模型切换按钮
+        modelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String model = (String) adapterView.getItemAtPosition(i);
+                Toast.makeText(MainActivity.this, "loading model: " + model, Toast.LENGTH_LONG).show();
+                initModel(model);
+                if(IS_FULL_SCREEN){
+                    cameraPreviewWrap.removeAllViews();
+                    FullScreenAnalyse fullScreenAnalyse = new FullScreenAnalyse(MainActivity.this,
+                            cameraPreviewMatch,
+                            boxLabelCanvas,
+                            rotation,
+                            inferenceTimeTextView,
+                            frameSizeTextView,
+                            yolov5TFLiteDetector);
+                    cameraProcess.startCamera(MainActivity.this, fullScreenAnalyse, cameraPreviewMatch);
+                }else{
+                    cameraPreviewMatch.removeAllViews();
+                    FullImageAnalyse fullImageAnalyse = new FullImageAnalyse(
+                            MainActivity.this,
+                            cameraPreviewWrap,
+                            boxLabelCanvas,
+                            rotation,
+                            inferenceTimeTextView,
+                            frameSizeTextView,
+                            yolov5TFLiteDetector);
+                    cameraProcess.startCamera(MainActivity.this, fullImageAnalyse, cameraPreviewWrap);
+                }
+
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // 监听视图变化按钮
+        immersive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                IS_FULL_SCREEN = b;
+                if (b) {
+                    // 进入全屏模式
+                    cameraPreviewWrap.removeAllViews();
+                    FullScreenAnalyse fullScreenAnalyse = new FullScreenAnalyse(MainActivity.this,
+                            cameraPreviewMatch,
+                            boxLabelCanvas,
+                            rotation,
+                            inferenceTimeTextView,
+                            frameSizeTextView,
+                            yolov5TFLiteDetector);
+                    cameraProcess.startCamera(MainActivity.this, fullScreenAnalyse, cameraPreviewMatch);
+
+                } else {
+                    // 进入全图模式
+                    cameraPreviewMatch.removeAllViews();
+                    FullImageAnalyse fullImageAnalyse = new FullImageAnalyse(
+                            MainActivity.this,
+                            cameraPreviewWrap,
+                            boxLabelCanvas,
+                            rotation,
+                            inferenceTimeTextView,
+                            frameSizeTextView,
+                            yolov5TFLiteDetector);
+                    cameraProcess.startCamera(MainActivity.this, fullImageAnalyse, cameraPreviewWrap);
+                }
+            }
+        });
+
+
+    }
+}
